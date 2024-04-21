d1d59d72e3c491f32cd6747de244526965e9b5fb
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2024-04-21 21:43:34 +0530

    new

--- app/src/main/java/com/example/logsignsqlpractice/MainActivity.java
+++ app/src/main/java/com/example/logsignsqlpractice/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.logsignsqlpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,54 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    public Button personaldetails;
+    public Button parkingspot;
+
+    public Button rentit;
+    public Button signout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        personaldetails=findViewById(R.id.personaldetails);
+        personaldetails.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent= new Intent(MainActivity.this,personaldetails.class);
+                    startActivity(intent);
+                }
+            });
+
+        parkingspot=findViewById(R.id.parkingspot);
+        parkingspot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,personaldetails.class);
+                startActivity(intent);
+            }
+        });
+
+        rentit=findViewById(R.id.rentit);
+        rentit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,personaldetails.class);
+                startActivity(intent);
+            }
+        });
+
+        signout=findViewById(R.id.signout);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,activity_login.class);
+                startActivity(intent);
+            }
+        });
+
+
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
